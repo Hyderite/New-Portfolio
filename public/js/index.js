70a91072241c074077419b1614ae4c98b25e20a6
Hyderite <hi@hyderite.dev>
2024-03-02 01:18:20 +0800

    Fixed animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,15 @@
 document.querySelector("#large-hyderite").style.display = "flex";
 setTimeout(function () {
-    document.querySelector("#large-hyderite").style.fontSize = "100px";
+    if (window.matchMedia("(max-width: 480px)").matches) {
+        document.querySelector("#large-hyderite").style.fontSize = "9.259vw";
+    } else {
+        document.querySelector("#large-hyderite").style.fontSize = "6vw";
+    };
     setTimeout(function () {
-        document.querySelector("#large-hyderite").style.transform = "translate(calc(-50% + (245.4px / 2 - 40px)), calc(-50% + 25px)) scale(0.15)";
+        if (window.matchMedia("(max-width: 480px)").matches) {
+            document.querySelector("#large-hyderite").style.transform = "translate(calc(-50% + (65vw / 2 - 3vw)), calc(-50% + 2.315vw + 20px)) scale(0.65)";
+        } else {
+            document.querySelector("#large-hyderite").style.transform = "translate(calc(-50% + (22.722vw / 2 - 5vw)), calc(-50% + 2.315vw)) scale(0.2)";
+        };
     }, 1000);
 }, 10);
