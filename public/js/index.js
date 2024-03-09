01121f41ba09abe8ef3a8c5ff3b31f19ddd1cd94
Hyderite <hi@hyderite.dev>
2024-03-09 23:20:20 +0800

    Hyderite changes from white to red

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,17 @@ setTimeout(function () {
     if (window.matchMedia("(max-width: 480px)").matches) {
         document.querySelector("#large-hyderite").style.fontSize = "9.259vw";
     } else {
-        document.querySelector("#large-hyderite").style.fontSize = "6vw";
+        document.querySelector("#large-hyderite").style.fontSize = "4vw";
     };
     setTimeout(function () {
-        if (window.matchMedia("(max-width: 480px)").matches) {
-            document.querySelector("#large-hyderite").style.transform = "translate(calc(-50% + (65vw / 2 - 3vw)), calc(-50% + 2.315vw + 20px)) scale(0.65)";
-        } else {
-            document.querySelector("#large-hyderite").style.transform = "translate(calc(-50% + (22.722vw / 2 - 5vw)), calc(-50% + 2.315vw)) scale(0.2)";
-        };
+        document.querySelector("#hyderite").style.color = "#c30000";
+        setTimeout(function () {
+            if (window.matchMedia("(max-width: 480px)").matches) {
+                document.querySelector("#large-hyderite").style.transform = "translate(calc(-50% + (65vw / 2 - 3vw)), calc(-50% + 2.315vw + 20px)) scale(0.65)";
+            } else {
+                document.querySelector("#large-hyderite").style.transform = "translate(calc(-50% + (22.722vw / 2 - 5vw)), calc(-50% + 2.315vw)) scale(0.3)";
+            };
+        }, 1000);
     }, 1000);
+
 }, 10);
