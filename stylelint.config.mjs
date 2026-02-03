6d67795115582cc865126e706bf25bed49e17912
Hyderite <hi@hyderite.dev>
2026-02-04 00:29:09 +0800

    chore: update stylelint config, add lightningcss config

--- stylelint.config.mjs
+++
@@ -1,9 +0,0 @@
-import standardConfig from 'stylelint-config-standard';
-
-/** @type {import('stylelint').Config} */
-export default {
-  extends: [standardConfig],
-  rules: {
-    'block-no-empty': true,
-  },
-};
