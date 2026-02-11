1ca1070dba594c2dd0563b8938037b3a8e39d589
Hyderite <hi@hyderite.dev>
2026-02-11 22:25:58 +0800

    feat: add marquee image grid background

--- public/src/ts/index.ts
+++ public/src/ts/index.ts
@@ -1,3 +1,97 @@
+const bgImages = [
+  'hk_taipo_lake_house.webp',
+  'hk_temple.webp',
+  'luna_on_laptop.webp',
+  'luna_blep.webp',
+  'luna_stretch.webp',
+  'luna_close.webp',
+  'hk_sky100.webp',
+  'hk_sky100_2.webp',
+  'hk_skyline.webp',
+  'hk_skyline_dusk.webp',
+  'hk_hiking_2.webp',
+  'hk_hiking.webp',
+  'seoul.webp',
+  'budapest_skyline_2.webp',
+  'budapest_skyline.webp',
+  'eger_castle.webp',
+  'krakow_wawel.webp',
+  'okinawa_sea.webp',
+  'okinawa_beach.webp',
+  'okinawa_manzamo.webp',
+  'okinawa_meadows.webp',
+  'CX_A333_ground.webp',
+  'B-KOO.webp',
+  'CX_B77W_takeoff.webp',
+  'LOT_Tu-134A.webp',
+  'pl_air_force_Yak-40.webp',
+  'cockpit_pl.webp',
+  'BA_B77L_oneworld_HKG.webp',
+  'B789_wing_inflight.webp',
+];
+
+let lastIndex = -1;
+
+const randomBg = (images: string[]): string => {
+  let newIndex: number;
+
+  do {
+    newIndex = Math.floor(Math.random() * images.length);
+  } while (newIndex === lastIndex && images.length > 1);
+
+  lastIndex = newIndex;
+  return images[newIndex];
+};
+
+const insertBg = () => {
+  const bg = document.querySelector<HTMLElement>('#bg');
+  if (!bg) return;
+
+  const minRowHeight = 250;
+  const screenHeight = window.innerHeight;
+  const rowCount = Math.max(2, Math.floor(screenHeight / minRowHeight));
+
+  bg.innerHTML = '';
+
+  for (let i = 0; i < rowCount; i++) {
+    const row = document.createElement('div');
+    row.className = 'row';
+
+    const imagesInRow: string[] = [];
+    const imagesSelected = 10;
+
+    for (let j = 0; j < imagesSelected; j++) {
+      imagesInRow.push(randomBg(bgImages));
+    }
+
+    const fullRow = [...imagesInRow, ...imagesInRow];
+
+    fullRow.forEach((src, index) => {
+      const img = document.createElement('img');
+      img.src = `https://cdn.hyderite.dev/img/portfolio/index-bg/${src}`;
+
+      if (index < 6) {
+        img.setAttribute('fetchpriority', 'high');
+        img.loading = 'eager';
+      } else {
+        img.loading = 'lazy';
+        img.setAttribute('fetchpriority', 'low');
+      }
+
+      img.decoding = 'async';
+      row.appendChild(img);
+    });
+
+    bg.appendChild(row);
+  }
+};
+
+window.addEventListener('resize', () => {
+  insertBg();
+});
+
+insertBg();
+
 const animation = {
   logo: document.querySelector<SVGElement>('#onload #logo')!,
   strokes: Array.from(document.querySelectorAll<SVGElement>('#onload .st0, #onload .st2, #onload .st3'))!,
@@ -25,6 +119,7 @@ window.addEventListener('load', async () => {
       });
     }
     if (dot) {
+      document.querySelector<HTMLElement>('#bg')!.style.opacity = '1';
       document.querySelector<HTMLElement>('#name')?.classList.add('draw');
       dot.style.opacity = '1';
       dot.style.transform = `translateX(0px)`;
